3b33c17eed4ef2362a17d200c68870af2cfb7442
Alan Morales <alan_jesus99@hotmail.com>
2020-08-25 20:22:46 -0500

    first commit

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+declare module "remark-html" {
+  const html: any;
+  export default html;
+}
+
+declare module "spotify-web-api-node" {
+  const SpotifyWebApi: any;
+  export default SpotifyWebApi;
+}
---
+++ pages/api/spotify/login.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import SpotifyWebApi from "spotify-web-api-node";
+import Cors from "cors";
+import runMiddleware from "../../../utils/runmiddleware";
+
+const scopes = ["user-read-private", "user-read-email"];
+const state = "loginin";
+
+const spotify = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+});
+
+const cors = Cors({
+  methods: ["GET"],
+});
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await runMiddleware(req, res, cors);
+  const authorizeURL = spotify.createAuthorizeURL(scopes, state);
+  res.redirect(authorizeURL);
+};

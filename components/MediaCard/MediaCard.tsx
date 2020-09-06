857143c14932ab659a483f3b8783434ecf41bbc8
Alan Morales <alan_jesus99@hotmail.com>
2020-09-06 13:11:52 -0500

    using container/display

--- components/MediaCard/MediaCard.tsx
+++ components/MediaCard/MediaCard.tsx
@@ -1,46 +1,29 @@
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import { FunctionComponent } from "react";
-
-const useStyles = makeStyles({
-  root: {
-    boxSizing: "border-box",
-    width: 200,
-    height: "100%",
-  },
-  media: {
-    height: 200,
-  },
-});
+import MediaCardDisplay from "./MediaCardDisplay";
 
 interface IProps {
-  track: any;
+  uri: string;
+  imageURL: string;
+  title: string;
+  description: string;
 }
 
-export const MediaCard: FunctionComponent<IProps> = ({ track }) => {
-  const classes = useStyles();
+export const MediaCard: FunctionComponent<IProps> = ({
+  uri,
+  imageURL,
+  title,
+  description,
+}) => {
+  const action = () => {
+    alert(`Reproduciendo: ${uri}`);
+  };
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={track.album.images[0].url}
-          title="Song Cover"
-        />
-        <CardContent>
-          <Typography variant="h6" component="h2">
-            {track.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {track.artists[0].name}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <MediaCardDisplay
+      imageURL={imageURL}
+      title={title}
+      description={description}
+      action={action}
+    />
   );
 };
---
+++ components/MediaCard/MediaCardDisplay.tsx
@@ -0,0 +1,51 @@
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { FunctionComponent } from "react";
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+    height: "100%",
+  },
+  media: {
+    height: 200,
+  },
+});
+
+interface IProps {
+  imageURL: string;
+  title: string;
+  description: string;
+  action: Function;
+}
+
+const MediaCardDisplay: FunctionComponent<IProps> = ({
+  imageURL,
+  title,
+  description,
+  action,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea onClick={() => action()}>
+        <CardMedia className={classes.media} image={imageURL} title={title} />
+        <CardContent>
+          <Typography variant="h6" component="h2">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default MediaCardDisplay;

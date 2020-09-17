4c918071458b08fe914085fb206b19f89ecf845c
Alan Morales <alan_jesus99@hotmail.com>
2020-09-17 11:52:33 -0500

    using CollectionNav

--- components/templates/CollectionsTemplate.tsx
+++ components/templates/CollectionsTemplate.tsx
@@ -1,12 +1,25 @@
 import { FunctionComponent } from "react";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, createStyles } from "@material-ui/core";
 import { SpotifyPlayer } from "../Player";
+import { CollectionNav } from "../Navs";
 
-const useStyles = makeStyles({
-  container: {
-    padding: "15px",
-  },
-});
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    container: {
+      padding: "15px",
+    },
+    nav: {
+      display: "flex",
+      gap: "15px",
+      height: "60px",
+      alignItems: "center",
+      position: "sticky",
+      top: "0px",
+      zIndex: 5,
+      backgroundColor: theme.palette.background.default,
+    },
+  })
+);
 
 interface IProps {
   children: any;
@@ -20,7 +33,7 @@ export const CollectionsTemplate: FunctionComponent<IProps> = ({
   return (
     <>
       <div className={classes.container}>
-        <p>here goes something else</p>
+        <CollectionNav initialPage="playlists" />
         {children}
       </div>
       <SpotifyPlayer />

72500bbe49c1ae233bff89fecf32e34732e58fc4
Alan Morales <alan_jesus99@hotmail.com>
2020-09-17 13:15:14 -0500

    deleted href prop

--- components/Buttons/NavButton.tsx
+++ components/Buttons/NavButton.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from "react";
 import { makeStyles, createStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import Link from "next/link";
 
 interface StyleProps {
   active?: boolean;
@@ -20,20 +21,20 @@ const useStyles = makeStyles((theme) =>
 );
 
 interface IProps {
-  href: string;
   active?: boolean;
+  onClick: Function;
   children: string;
 }
 
 export const NavButton: FunctionComponent<IProps> = ({
-  href,
   active,
+  onClick,
   children,
 }) => {
   const classes = useStyles({ active: active });
 
   return (
-    <Button className={classes.button} component="a" href={href}>
+    <Button className={classes.button} onClick={() => onClick()}>
       {children}
     </Button>
   );

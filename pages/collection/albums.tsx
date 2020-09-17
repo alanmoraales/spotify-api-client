804d569e5099a49dfd837382deb5ee8bd5e46d52
Alan Morales <alan_jesus99@hotmail.com>
2020-09-16 23:50:24 -0500

    using collection template

--- pages/collection/albums.tsx
+++ pages/collection/albums.tsx
@@ -1,11 +1,9 @@
 import { useState, useEffect, useContext } from "react";
 import { NextPage } from "next";
 import { SpotifyContext } from "../../components/SpotifyContext";
-import { makeStyles, Typography } from "@material-ui/core";
 import { GridList } from "../../components/GridList";
 import { AlbumCard } from "../../components/Cards";
-import { SpotifyPlayer } from "../../components/Player";
-import { useRouter } from "next/dist/client/router";
+import { CollectionTemplate } from "../../components/templates";
 
 const useUserAlbums = (initialState: any, params: Object | undefined) => {
   const [albums, setAlbums] = useState(initialState);
@@ -25,43 +23,17 @@ const useUserAlbums = (initialState: any, params: Object | undefined) => {
   return [albums];
 };
 
-const useStyles = makeStyles({
-  container: {
-    padding: "15px",
-  },
-  title: {
-    paddingTop: "30px",
-    paddingBottom: "15px",
-  },
-});
-
 const Albums: NextPage = () => {
-  const { loggedIn } = useContext(SpotifyContext);
   const [albums] = useUserAlbums({ items: [] }, { limit: 30 });
-  const router = useRouter();
-
-  const classes = useStyles();
-
-  useEffect(() => {
-    if (!loggedIn) {
-      router.push("/");
-    }
-  }, []);
 
   return (
-    <>
-      <div className={classes.container}>
-        <Typography variant="h5" className={classes.title}>
-          Albums
-        </Typography>
-        <GridList>
-          {albums.items.map((album: any) => (
-            <AlbumCard album={album.album} key={album.album.uri} />
-          ))}
-        </GridList>
-      </div>
-      <SpotifyPlayer />
-    </>
+    <CollectionTemplate title="Albums">
+      <GridList>
+        {albums.items.map((album: any) => (
+          <AlbumCard album={album.album} key={album.album.uri} />
+        ))}
+      </GridList>
+    </CollectionTemplate>
   );
 };
 

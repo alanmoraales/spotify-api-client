d69ae182f5da36b9abb171ecd4cd2229e8658486
Alan Morales <alan_jesus99@hotmail.com>
2020-09-17 15:37:20 -0500

    adding data

--- components/Pages/Playlists.tsx
+++ components/Pages/Playlists.tsx
@@ -1,11 +1,40 @@
-import { FunctionComponent, useContext } from "react";
+import { FunctionComponent, useContext, useState, useEffect } from "react";
+import { PlaylistCard } from "../Cards";
 import { CollectionContext } from "../CollectionContext";
+import { GridList } from "../GridList";
+import { SpotifyContext } from "../SpotifyContext";
 import { CollectionTemplate } from "../templates";
 
+const useUserPlaylists = (initialState: any, params: Object | undefined) => {
+  const [playlists, setPlaylists] = useState(initialState);
+  const { spotify, loggedIn, fetchUserData } = useContext(SpotifyContext);
+
+  const fetchPlaylists = async () => {
+    const playlists = await fetchUserData(spotify.getUserPlaylists, params);
+    console.log(playlists);
+    setPlaylists(playlists);
+  };
+
+  useEffect(() => {
+    if (loggedIn) {
+      fetchPlaylists();
+    }
+  }, [loggedIn]);
+
+  return [playlists];
+};
+
 export const Playlists: FunctionComponent = () => {
+  const [playlists] = useUserPlaylists({ items: [] }, { limit: 30 });
   const { state } = useContext(CollectionContext);
 
   return state.onPlaylistsPage ? (
-    <CollectionTemplate title="Playlists">playlists</CollectionTemplate>
+    <CollectionTemplate title="Playlists">
+      <GridList>
+        {playlists.items.map((playlist: any) => (
+          <PlaylistCard playlist={playlist} key={playlist.id} />
+        ))}
+      </GridList>
+    </CollectionTemplate>
   ) : null;
 };
